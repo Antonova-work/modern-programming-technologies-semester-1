3543db4910e1e856fd3edf130dc4c1fca2312ae0
Anastasiya <aadworkmaill@gmail.com>
2026-02-11 12:23:18 +0300

    change the output text

--- src/lesson1/HelloWorld.java
+++ src/lesson1/HelloWorld.java
@@ -2,6 +2,6 @@ package lesson1;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld!");
+        System.out.println("A little change");
     }
 }
