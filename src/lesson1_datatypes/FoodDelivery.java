f45088a44267f0384750b1d4d3837dea367ddc6c
Anastasiya <aadworkmaill@gmail.com>
2026-02-13 17:03:01 +0300

    add exercise on working with branches

--- src/lesson1_datatypes/FoodDelivery.java
+++ src/lesson1_datatypes/FoodDelivery.java
@@ -1,4 +1,4 @@
-package lesson1;
+package lesson1_datatypes;
 
 public class FoodDelivery {
     public static void main (String[] args){
---
+++ src/lesson2_branches/LuckyOrder.java
@@ -0,0 +1,12 @@
+package lesson2_branches;
+
+public class LuckyOrder {
+    public static void main(String[] args) {
+        int deliveryNumber = 777;
+        if (deliveryNumber == 777) {
+            System.out.println("Вы выиграли приз!");
+        } else {
+            System.out.println("Повезёт в другой раз!");
+        }
+    }
+}
