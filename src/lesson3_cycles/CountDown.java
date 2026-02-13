8e0c9f649fd367251c8403bdc4bd5f60e60e7661
Anastasiya <aadworkmaill@gmail.com>
2026-02-13 17:21:38 +0300

    add exercise on working with cycles

---
+++ src/lesson3_cycles/CountDown.java
@@ -0,0 +1,10 @@
+package lesson3_cycles;
+
+public class CountDown {
+    public static void main(String[] args) {
+        for (int i = 5; i > 0; i--) {
+            System.out.println(i);
+        }
+        System.out.println("Заказ готов!");
+    }
+}
